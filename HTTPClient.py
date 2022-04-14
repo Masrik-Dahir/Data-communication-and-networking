c776a44610ca57fd5665c9bb13d502d991e8294f
masri <dahirma@vcu.edu>
2022-04-14 12:52:38 -0400

    new
    Signed-off-by: Masrik Dahir <dahirma@vcu.edu>

--- HTTPClient.py
+++ HTTPClient.py
@@ -3,6 +3,29 @@
 
 # Sample input: GET /index.html HTTP/1.0
 
+# p = '(?:http.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*)(?P<path>[^\s]*)'
+# link = re.search(p, url)
+#
+# host_name = link.group('host')
+# print("host= " + host_name)
+#
+# port_name = link.group('port')
+# if port_name == "":
+#     port_name = "80"
+#     addSlash = True
+# print("port= " + port_name)
+#
+# path_name = link.group('path')
+# if addSlash:
+#     path_name = "/" + path_name
+# if path_name == "":
+#     path_name = "/"
+# print("path= " + path_name)
+
+
+
+
+
 import socket
 server_name = '127.0.0.1'
 server_port = 8000
