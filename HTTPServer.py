08871f6e86da8aff14a4a6f3a4784d468c62e975
Masrik Dahir <dahirma@vcu.edu>
2022-04-20 02:27:28 -0400

    done hopefully
    Signed-off-by: Masrik Dahir <dahirma@vcu.edu>

--- HTTPServer.py
+++ HTTPServer.py
@@ -14,61 +14,66 @@ def semicolon(st:str, var: str):
     arg = re.sub("^\s+","", arg)
     return arg
 
-try:
-    if len(sys.argv) == 2 and int(sys.argv[1])>0 and int(sys.argv[1]) < 65536:
-            server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_port = int(sys.argv[1])
-            server.bind(('', server_port))
-            server.listen(1)
-    else:
-        ag = ""
-        for i in range(1,len(sys.argv)):
-            ag += str(i) + " "
-        sys.exit("ERR -arg %s" %(ag))
-except:
-        ag = ""
-        for i in range(1, len(sys.argv)):
-            ag += str(i) + " "
-        sys.exit("ERR -arg %s" % (ag))
-
-
-print ("The server is ready to receive at port: %s" %(server_port))
-while 1:
-    print ("Waiting ...")
-    connection_socket, addr = server.accept()
-    print ("accept\n")
-    sentence = connection_socket.recv(2048).decode()
-    request_type = str(sentence.split("\n\r")[0].split(" ")[0])
-
-    # Printing client's ip + port + request type
-    print(str(addr[0]) + ":" + str(addr[1]) + ":" + request_type)
-
-    # Printing ech line in HTTP body
-    print(sentence)
-
-
-    # Get the content of htdocs/index.html
+def main():
     try:
-        filename = sentence.split()[1]
-        lines = ""
-        with open(filename) as f:
-            lines = f.read()
-        print(lines)
-        directory = './'
-        file_path = os.path.join(directory, filename.split("/")[-1])
-        if not os.path.isdir(directory):
-            os.mkdir(directory)
-        file = open(file_path, "w")
-        file.write(lines)
-        file.close()
-
-        # Printing valid HTTP response
-        response = 'HTTP/1.0 200 OK\r\nFile: %s\r\n\r\n' % (filename)
-
-    except FileNotFoundError:
-        response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
+        if len(sys.argv) == 2 and int(sys.argv[1])>0 and int(sys.argv[1]) < 65536:
+                server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server_port = int(sys.argv[1])
+                server.bind(('', server_port))
+                server.listen(1)
+        else:
+            ag = ""
+            for i in range(1,len(sys.argv)):
+                ag += str(i) + " "
+            sys.exit("ERR -arg %s" %(ag))
+    except:
+            ag = ""
+            for i in range(1, len(sys.argv)):
+                ag += str(i) + " "
+            sys.exit("ERR -arg %s" % (ag))
 
 
-    modifiedSentence = 'Message Received!'
-    connection_socket.send(response.encode('utf-8'))
-    connection_socket.close()
+    print ("The server is ready to receive at port: %s" %(server_port))
+
+    while 1:
+        print ("Waiting ...")
+        connection_socket, addr = server.accept()
+        print ("accept\n")
+        sentence = connection_socket.recv(2048).decode()
+        request_type = str(sentence.split("\n\r")[0].split(" ")[0])
+
+        # Printing client's ip + port + request type
+        print(str(addr[0]) + ":" + str(addr[1]) + ":" + request_type)
+
+        # Printing ech line in HTTP body
+        print(sentence)
+
+
+        # Get the content of htdocs/index.html
+        try:
+            filename = sentence.split()[1]
+            lines = ""
+            with open(filename) as f:
+                lines = f.read()
+            print(lines)
+            directory = './'
+            file_path = os.path.join(directory, filename.split("/")[-1])
+            if not os.path.isdir(directory):
+                os.mkdir(directory)
+            file = open(file_path, "w")
+            file.write(lines)
+            file.close()
+
+            # Printing valid HTTP response
+            response = 'HTTP/1.0 200 OK File Created'
+
+        except FileNotFoundError:
+            response = 'HTTP/1.0 606 File NOT Created'
+
+
+        modifiedSentence = 'Message Received!'
+        connection_socket.send(response.encode('utf-8'))
+        connection_socket.close()
+
+
+
